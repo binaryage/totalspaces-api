e2dcd1d75118fe3becbef5fd05a83ad18ac0d5cf
Stephen Sykes <sdsykes@gmail.com>
2013-02-20 14:33:42 +0200

    Adds move spaces calls, and improves documentation

--- ruby/lib/TSLib.h
+++ ruby/lib/TSLib.h
@@ -13,24 +13,29 @@
 
 #import <Foundation/Foundation.h>
 
+#define TSAPI_MAX_SPACES 16
+
 /*
  * In case of comm error, all the functions apart from tsapi_libTotalSpacesVersion() will 
  * return an empty string, zero, false or a pointer to a struct containing zero spaces (in 
  * the case of tsapi_windowList).
  * It is recommended to check that comms to TotalSpaces are working by, for instance,
- * checking that tsapi_numberOfSpaces() returns a number greater than zero when you initialize
+ * checking that tsapi_apiVersion() matches tsapi_libTotalSpacesVersion() when you initialize
  * your app.
+ *
+ * Some actions such as renaming a space will cause the overview grid to be exited if it
+ * is showing at the time.
  */
 
 /*
- * The version of the API present in TotalSpaces.app
+ * The version of the API present in TotalSpaces.app.
  *
  * You must call tsapi_freeString when you have finished with the returned string.
  */
 const char *tsapi_apiVersion();
 
 /*
- * The version number of TotalSpaces itself
+ * The version number of TotalSpaces itself.
  *
  * You must call tsapi_freeString when you have finished with the returned string.
  */
@@ -38,90 +43,110 @@ const char *tsapi_totalSpacesVersion();
 
 /*
  * The version of the API dylib. This should match the string returned
- * by tsapi_apiVersion()
+ * by tsapi_apiVersion().
  *
  * You must call tsapi_freeString when you have finished with the returned string.
  */
 const char *tsapi_libTotalSpacesVersion();
 
 /*
- * The number of the current space
+ * The number of the current space.
  *
  * If the current space is the dashboard, 0 is returned.
  */
 unsigned int tsapi_currentSpaceNumber();
 
 /*
- * The name for the given space number
+ * The name for the given space number.
  *
  * You must call tsapi_freeString when you have finished with the returned string.
  */
 const char *tsapi_spaceNameForSpaceNumber(unsigned int spaceNumber);
 
 /*
- * The total number of spaces
+ * The total number of spaces.
  * This includes the dashboard if you have it set as a space, and any
- * fullscreen apps
+ * fullscreen apps.
  */
 unsigned int tsapi_numberOfSpaces();
 
 /*
- * The number of fullscreen apps
+ * The number of fullscreen apps.
  */
 unsigned int tsapi_numberOfFullScreens();
 
 /*
- * The number of desktops / normal spaces
+ * The number of desktops / normal spaces.
  */
 unsigned int tsapi_numberOfDesktops();
 
 /*
  * The number of fullscreen apps that are allowed to be present
- * in the grid, defined in advanced preferences
+ * in the grid, defined in advanced preferences.
  */
 unsigned int tsapi_numberOfFullScreensInGrid();
 
 /*
- * Is the dashboard set to be a space in Mission Control preferences
+ * Is the dashboard set to be a space in Mission Control preferences.
  */
 bool tsapi_dashboardIsASpace();
 
 /*
- * The number of rows defined in TotalSpaces layout preferences
+ * The number of rows defined in TotalSpaces layout preferences.
  */
 unsigned int tsapi_definedRows();
 
 /*
- * The number of columns defined in TotalSpaces layout preferences
+ * The number of columns defined in TotalSpaces layout preferences.
  */
 unsigned int tsapi_definedColumns();
 
 /*
- * Call this to free strings returned by the TotalSpaces API
+ * Sets the number of rows in the TotalSpaces grid.
+ * Returns true on success, false if the new grid would exceed TSAPI_MAX_SPACES
+ * or if rows is zero.
+ * Note that the actual number of desktops present in the system is unchanged,
+ * you should call tsapi_addDesktops or tsapi_removeDesktops after calling this
+ * function.
+ */
+bool tsapi_setDefinedRows(unsigned int rows);
+
+/*
+ * Sets the number of columns in the TotalSpaces grid.
+ * Returns true on success, false if the new grid would exceed TSAPI_MAX_SPACES
+ * or if columns is zero.
+ * Note that the actual number of desktops present in the system is unchanged,
+ * you should call tsapi_addDesktops or tsapi_removeDesktops after calling this
+ * function.
+ */
+bool tsapi_setDefinedColumns(unsigned int columns);
+
+/*
+ * Call this to free strings returned by the TotalSpaces API.
  */
 void tsapi_freeString(char *str);
 
 /*
- * Switch the display to the given space
- * Returns false if the space number is invalid
+ * Switch the display to the given space.
+ * Returns false if the space number is invalid.
  */
 bool tsapi_moveToSpace(unsigned int spaceNumber);
 
 /*
- * Set the name of a space
- * The maximum length is 255 bytes. The name should be in UTF-8
- * Returns true on success, false if the name was too long or the space number was invalid
+ * Set the name of a space.
+ * The maximum length is 255 bytes. The name should be in UTF-8.
+ * Returns true on success, false if the name was too long or the space number was invalid.
  */
 bool tsapi_setNameForSpace(unsigned int spaceNumber, char *name);
 
 /*
- * Type for space change callback
+ * Type for space change callback.
  */
 typedef void (*space_change_callback_t)(unsigned int fromSpaceNumber, unsigned int toSpaceNumber);
 
 /*
- * Set the function that will be called when the visible space changes
- * There is only one callback per process, registering a new callback will supercede any previous one
+ * Set the function that will be called when the visible space changes.
+ * There is only one callback per process, registering a new callback will supercede any previous one.
  */
 void tsapi_setSpaceWillChangeCallback(space_change_callback_t callback);
 
@@ -136,21 +161,21 @@ void tsapi_unsetSpaceWillChangeCallback();
 typedef void (*space_layout_changed_callback_t)(void);
 
 /*
- * Set the function that will be called when the layout changes
+ * Set the function that will be called when the layout changes.
  * This could be any change - for instance adding or removing a fullscreen, changing the name of a space,
  * or a change of rows or columns.
- * It indicates that you should re-request any information you are holding on the spaces
- * There is only one callback per process, registering a new callback will supercede any previous one
+ * It indicates that you should re-request any information you are holding on the spaces.
+ * There is only one callback per process, registering a new callback will supercede any previous one.
  */
 void tsapi_setLayoutChangedCallback(space_layout_changed_callback_t callback);
 
 /*
- * Cancel layout change callbacks
+ * Cancel layout change callbacks.
  */
 void tsapi_unsetLayoutChangedCallback();
 
 /*
- * Struct containing information about a window
+ * Struct containing information about a window.
  */
 struct tsapi_window {
   char *appName;
@@ -161,8 +186,8 @@ struct tsapi_window {
 };
 
 /*
- * Struct containing information about a space
- * Contains a pointer to an array of tsapi_window structs
+ * Struct containing information about a space.
+ * Contains a pointer to an array of tsapi_window structs.
  */
 struct tsapi_space {
   unsigned int spaceNumber;
@@ -172,7 +197,7 @@ struct tsapi_space {
 
 /*
  * Struct containing the count of spaces and a pointer to an
- * array of tsapi_space structs
+ * array of tsapi_space structs.
  */
 struct tsapi_spaces {
   unsigned int spacesCount;
@@ -181,11 +206,11 @@ struct tsapi_spaces {
 
 /*
  * Return a pointer to a tsapi_spaces struct containing information about all the windows
- * in all spaces
+ * in all spaces.
  *
- * The windows are listed front to back, so the first widow in the array is the frontmost
+ * The windows are listed front to back, so the first widow in the array is the frontmost.
  *
- * You must call tsapi_freeWindowList when you have finished with this
+ * You must call tsapi_freeWindowList when you have finished with this.
  */
 struct tsapi_spaces *tsapi_windowList();
 
@@ -201,6 +226,41 @@ void tsapi_freeWindowList(struct tsapi_spaces *windowList);
  * Returns true on success, false if the windowId or spaceNumber was invalid
  */
 bool tsapi_moveWindowToSpace(unsigned int windowId, unsigned int spaceNumber);
+
+/*
+ * Move a space to another position
+ * You cannot move a space to position 1, and you cannot move the first
+ * space anywhere else - it is fixed.
+ * You cannot move full screen apps around with this method, only normal desktops.
+ *
+ * Returns true on success, false if the spaceNumber or positionNumber was
+ * invalid
+ */
+bool tsapi_moveSpaceToPosition(unsigned int spaceNumber, unsigned int positionNumber);
+
+/*
+ * Add desktops
+ * There can be at most 16 desktops
+ *
+ * Returns true on success, false if numberToAdd was zero, or would result in more than
+ * 16 desktops
+ */
+bool tsapi_addDesktops(unsigned int numberToAdd);
+
+/*
+ * Remove desktops
+ * Removes numberToRemove desktops. The highest numbered desktops are removed.
+ *
+ * Removing a desktop you are currently on will result in TotalSpaces switching to
+ * another dektop.
+ *
+ * Any windows present on a desktop being removed will be moved to one of the
+ * remaining desktops.
+ *
+ * Returns true on success, false if numberToRemove was zero or would result in less
+ * than 1 desktop remaining.
+ */
+bool tsapi_removeDesktops(unsigned int numberToRemove);
 
 #endif
 
